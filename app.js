04104029a5af396e349cedc42cef70b3815575c5
Jon Veach <veach09@gmail.com>
2019-07-12 16:03:25 -0700

    Completed problem4

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   var addSum = a + b;
   var answer = 'The sum of ' + a + ' and ' + b + ' is ' + addSum + '.';
-
+  // console.log(`addSum = ${addSum}, answer = ${answer}`);
   return [addSum, answer];
 }
 
@@ -79,13 +79,21 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
 
+function sumArray(sumArr) { //eslint-disable-line
+  var x = sum(sumArr[0],sumArr[1]); // 2 + 3 = 5
+  var y = sum(x[0], sumArr[2]); // 0 + 4 << 5 + 4
+  var summedArr = y[0];
+
+  var answer = testArray + ' was passed in as an array of numbers, and ' + summedArr + ' is their sum.';
+
+  return [summedArr, answer];
 }
 
+sumArray(testArray);
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
